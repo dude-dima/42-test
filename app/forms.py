e138e019c0739523e3e8225c786061ba18b139be
dude-dima <dude-serg@yandex.ru>
2011-01-17 16:25:50 +0200

    ticket:7 impl

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,10 @@ from models import Customer
 from widgets import JQCalendarWidget
 
 class CustomerForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kw):
+        super(forms.ModelForm, self).__init__(*args, **kw)
+        self.fields.keyOrder.reverse()
 
     class Meta:
         model = Customer
