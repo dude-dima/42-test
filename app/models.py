dbe8995d83d341b7a0371a5f5cc28d8bfb08aa08
dude-dima <dude-serg@yandex.ru>
2011-01-18 10:03:46 +0200

    ticket:3 impl3

--- app/models.py
+++ app/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
-class User(models.Model):
-    """User"""
+
+class Customer(models.Model):
+    """Customer"""
     name = models.CharField(null=True, max_length=250)
     surname = models.CharField(null=True, max_length=250)
     bio = models.TextField(null=True, max_length=250)
     contacts = models.TextField(null=True, max_length=250)
-    
+
+    def __unicode__(self):
+        return self.name
+
     def get_data(self):
         return [(field.name, field.value_to_string(self))\
                 for field in self._meta.fields]
-            
-    def __str__(self):
-        return self.name
-    
+
     class Meta:
         db_table = 'users'
-        
+
 
 class Request(models.Model):
     """Request"""
