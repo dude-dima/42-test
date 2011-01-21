13669e478fb66110b2d5738783fb2a25c3ec2e04
dude-dima <dude-serg@yandex.ru>
2011-01-21 12:53:32 +0200

    ticket:1 impl6

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Customer(models.Model):
     """Customer"""
-    name = models.CharField(null=True, blank=True, max_length=250)
-    surname = models.CharField(null=True, blank=True, max_length=250)
-    bio = models.TextField(null=True, blank=True, max_length=250)
-    contacts = models.TextField(null=True, blank=True, max_length=250)
+    name = models.CharField(max_length=250)
+    surname = models.CharField(max_length=250)
+    bio = models.TextField(max_length=250)
+    contacts = models.TextField(max_length=250)
 
     class Meta:
         db_table = 'users'
