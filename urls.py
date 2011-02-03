fc33c7978ae84cd60276b8457c50e0a7c716ff92
dude-dima <dude-serg@yandex.ru>
2011-02-03 16:04:01 +0200

    ticket:7 impl3

--- urls.py
+++ urls.py
@@ -1,28 +1,20 @@
 from django.conf.urls.defaults import patterns, include
 import os
 
-# Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
 
 urlpatterns = patterns('',
+
     (r'', include('app.urls')),
 
     (r'^css/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': os.path.join(os.path.dirname(__file__),
-                                       "templates/css").replace('\\','/') }),
+                                       "templates/css").replace('\\', '/')}),
 
     (r'^js/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': os.path.join(os.path.dirname(__file__),
-                                       "templates/js").replace('\\','/') }),
+                                       "templates/js").replace('\\', '/')}),
 
     (r'^images/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': os.path.join(os.path.dirname(__file__),
-                                       "templates/images").replace('\\','/') }),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #(r'^admin/', include(admin.site.urls)),
+                                    "templates/images").replace('\\', '/')}),
 )
